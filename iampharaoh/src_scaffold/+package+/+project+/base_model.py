c9ca77cab1e947af0acbc3cff9688cfd89e8b399
Mario Idival <marioidival@gmail.com>
2014-11-02 00:29:30 -0300

    change models and pep8;

--- iampharaoh/src_scaffold/+package+/+project+/base_model.py
+++ iampharaoh/src_scaffold/+package+/+project+/base_model.py
@@ -1,11 +1,30 @@
-from sqlalchemy.ext.declarative import declarative_base
-
-from sqlalchemy.orm import (
-        scoped_session,
-        sessionmaker,
-        )
+import re
+from sqlalchemy.ext.declarative import declarative_base, declared_attr
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy import Column, Integer
 
 from zope.sqlalchemy import ZopeTransactionExtension
 
+
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
-Base = declarative_base()
+
+
+class PharaohBase(object):
+    """
+    Gives PK and table name for future classes
+    """
+
+    @declared_attr
+    def pk(self):
+        return Column(Integer, primary_key=True)
+
+    @declared_attr
+    def __tablename__(cls):
+        name = cls.__name__.replace('Mixin', '')
+
+        return (
+            name[0].lower() +
+            re.sub(r'([A-Z])', lambda m: "_" + m.group(0).lower(), name[1:])
+        )
+
+Base = declarative_base(cls=PharaohBase)
